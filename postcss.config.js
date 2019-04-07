4b7c0865be9885e89c8201e21e57b73311a0b8e8
Djuuu <julien@tardot.net>
2019-04-07 19:28:01 +0200

    Style - use Purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,24 @@
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+        './public/*.html',
+        './src/*.vue',
+        './src/**/*.vue',
+    ],
+    extractors: [{
+        extractor: class {
+            static extract(content) {
+                return content.match(/[A-Za-z0-9-_:/]+/g) || [];
+            }
+        },
+        extensions: ['html', 'vue']
+    }]
+});
+
 module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
+        process.env.NODE_ENV === 'production' ? purgecss : null,
     ]
 };
