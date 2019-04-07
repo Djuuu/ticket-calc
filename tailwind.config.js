9009b4e7f8d96b3dcdcd92f2dfbb0d55b9726116
Djuuu <julien@tardot.net>
2019-04-07 19:43:36 +0200

    Style - limit color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,35 @@
 module.exports = {
     theme: {
-        extend: {
-            colors: {
-                primary:    '#e3001b',
-                secondary:  '#0f204b',
-                danger:     '#f53d3d',
-                light:      '#f2f2f2',
-                dark:       '#222',
+        colors: {
+            transparent: 'transparent',
 
-                positive:   '#a40000',
-                negative:   '#668000',
-                equal:      '#008000',
+            black: '#000',
+            white: '#fff',
 
-                positiveBg: '#798000',
-                negativeBg: '#cc001b',
-                equalBg:    '#009900',
+            primary:    '#e3001b',
+            secondary:  '#0f204b',
+            danger:     '#f53d3d',
+            light:      '#f2f2f2',
+            dark:       '#222',
 
-                gray:   {
-                    100: '#fafafa',
-                    200: '#f5f5f5',
-                    300: '#f0f0f0',
-                    400: '#dedede',
-                    500: '#bfbfbf',
-                    600: '#949494',
-                    700: '#666666',
-                    800: '#474747',
-                    900: '#2b2b2b',
-                },
+            positive:   '#a40000',
+            negative:   '#668000',
+            equal:      '#008000',
+
+            positiveBg: '#798000',
+            negativeBg: '#cc001b',
+            equalBg:    '#009900',
+
+            gray: {
+                100: '#fafafa',
+                200: '#f5f5f5',
+                300: '#f0f0f0',
+                400: '#dedede',
+                500: '#bfbfbf',
+                600: '#949494',
+                700: '#666666',
+                800: '#474747',
+                900: '#2b2b2b',
             },
         },
         screens: {
